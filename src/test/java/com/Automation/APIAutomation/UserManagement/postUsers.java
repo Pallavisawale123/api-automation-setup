aebc5d74565a5ab6dbc9f4ec562fa5492a570fa4
Pallavisawale123 <pallavisawale11@gmail.com>
2023-12-21 01:41:35 +0530

    updated test cases as per reviwed comments

--- src/test/java/com/Automation/APIAutomation/UserManagement/postUsers.java
+++ src/test/java/com/Automation/APIAutomation/UserManagement/postUsers.java
@@ -1,20 +1,31 @@
 package com.Automation.APIAutomation.UserManagement;
 
 import com.Automation.APIAutomation.ApiAutomationApplication;
+import com.Automation.APIAutomation.automationReport.BaseTestExtentReport;
 import com.Automation.APIAutomation.automationReport.ExtentReport;
 import com.Automation.APIAutomation.enums.StatusCode;
 import com.Automation.APIAutomation.helper.AssertHelper;
 import com.Automation.APIAutomation.helper.TestListeners;
+import com.Automation.APIAutomation.utils.PropertyReader;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.IOUtils;
+import org.json.simple.JSONObject;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+
 import static io.restassured.RestAssured.given;
 
 /**
@@ -24,8 +35,35 @@ import static io.restassured.RestAssured.given;
 @Slf4j
 @Listeners(TestListeners.class)
 
-public final class postUsers {
+public final class postUsers extends BaseTestExtentReport {
     private static FileInputStream fileInputStream;
+    String serverAddress = PropertyReader.propertyReader("config.properties", "serverAddress");
+
+    ResponseSpecBuilder builder;
+    ResponseSpecification responseSpecification;
+
+    /**
+     * Common method to save response
+     *
+     * @return
+     */
+    public ResponseSpecification responseSpec() {
+        ResponseSpecBuilder builder1 = new ResponseSpecBuilder();
+        builder1.expectContentType(ContentType.JSON);
+        return builder1.build();
+    }
+
+    /**
+     * Common method to send a request
+     *
+     * @return
+     */
+    public RequestSpecification requestSpec() {
+        RequestSpecBuilder builder = new RequestSpecBuilder();
+        builder.setBaseUri(serverAddress);
+        builder.setContentType(ContentType.JSON);
+        return builder.build();
+    }
 
     /**
      * Method to return the given json file
@@ -53,14 +91,31 @@ public final class postUsers {
                 ExtentReport.extentreport.
                         startTest("validatePostWithString",
                                 "ValidatevalidatePostWithString");
-        Response response = given()
+        JSONObject requestParams = new JSONObject();
+        requestParams.put("name", "pallavi");
+        requestParams.put("job", "QA");
+
+//        Response response = given()
+//                .header("Content-Type", "application/json")
+//                .body(requestParams.toJSONString())
+//                .when()
+//                .post("https://reqres.in/api/users");
+
+        Response response = (Response) given().spec(requestSpec())
                 .header("Content-Type", "application/json")
-                .body("{\"name\":\"pallavi\",\"job\":\"QA\"}")
-                .when()
-                .post("https://reqres.in/api/users");
-        AssertHelper.assertEquals(response.getStatusCode(), StatusCode.CREATED.code, "");
+                .body(requestParams.toJSONString())
+                .post().then().spec(responseSpec()).extract();
+
+        AssertHelper.assertEquals(response.getStatusCode(), StatusCode.CREATED.code, "Verifying status code " +
+                "for POST API");
         log.info("validatePostWithString executed successfully");
         log.info(response.getBody().asString());
+        JsonPath jsonPath = new JsonPath(response.getBody().asString());
+        jsonPath.getString("job");
+        log.info(jsonPath.getString("job"));
+        AssertHelper.assertEquals(jsonPath.getString("job"), "QA",
+                "Verifying job profile ");
+
     }
 
     /**
@@ -72,14 +127,27 @@ public final class postUsers {
                 ExtentReport.extentreport.
                         startTest("validatePutWithString",
                                 "validatePutWithString");
-        Response response = given()
+        JSONObject requestParams = new JSONObject();
+        requestParams.put("name", "pallavisawale");
+        requestParams.put("job", "SDET");
+
+//        Response response = given()
+//                .header("Content-Type", "application/json")
+//                .body(requestParams.toJSONString())
+//                .when()
+//                .put("https://reqres.in/api/users/2");
+        Response response = (Response) given().spec(requestSpec())
                 .header("Content-Type", "application/json")
-                .body("{\"name\":\"pallavisawale\",\"job\":\"SDET\"}")
-                .when()
-                .put("https://reqres.in/api/users/2");
+                .body(requestParams.toJSONString())
+                .put("/2").then().spec(responseSpec()).extract();
+
         AssertHelper.assertEquals(response.getStatusCode(), StatusCode.SUCCESS.code, "");
         log.info("validatePutWithString executed successfully");
-        log.info(response.getBody().asString());
+        JsonPath jsonPath = new JsonPath(response.getBody().asString());
+        jsonPath.getString("job");
+        log.info(jsonPath.getString("job"));
+        AssertHelper.assertEquals(jsonPath.getString("job"), "SDET",
+                "Verifying job profile ");
     }
 
     /**
@@ -91,14 +159,29 @@ public final class postUsers {
                 ExtentReport.extentreport.
                         startTest("validatePatchWithString",
                                 "validatePatchWithString");
-        Response response = given()
+        JSONObject requestParams = new JSONObject();
+        requestParams.put("name", "SawalePallavi");
+
+//        Response response = given()
+//                .header("Content-Type", "application/json")
+//                .body(requestParams.toJSONString())
+//                .when()
+//                .patch("https://reqres.in/api/users/2");
+
+        Response response = (Response) given().spec(requestSpec())
                 .header("Content-Type", "application/json")
-                .body("{\"name\":\"SawalePallavi\"}")
-                .when()
-                .patch("https://reqres.in/api/users/2");
+                .body(requestParams.toJSONString())
+                .patch("/2").then().spec(responseSpec()).extract();
+
         AssertHelper.assertEquals(response.getStatusCode(), StatusCode.SUCCESS.code, "");
         log.info("validatePatchWithString executed successfully");
         log.info(response.getBody().asString());
+        log.info("validatePutWithString executed successfully");
+        JsonPath jsonPath = new JsonPath(response.getBody().asString());
+        jsonPath.getString("name");
+        log.info(jsonPath.getString("name"));
+        AssertHelper.assertEquals(jsonPath.getString("name"), "SawalePallavi",
+                "Verifying name of employee");
     }
 
     /**
@@ -110,15 +193,26 @@ public final class postUsers {
                 ExtentReport.extentreport.
                         startTest("validatePostWithJsonFile",
                                 "validatePostWithJsonFile");
-        Response response = given()
+//        Response response = given()
+//                .header("Content-Type", "application/json")
+//                .body(IOUtils.toString(fileInputStreamMethod("postRequestBody.json")))
+//                .when()
+//                .post("https://reqres.in/api/users");
+
+        Response response = (Response) given().spec(requestSpec())
                 .header("Content-Type", "application/json")
                 .body(IOUtils.toString(fileInputStreamMethod("postRequestBody.json")))
-                .when()
-                .post("https://reqres.in/api/users");
+                .post().then().spec(responseSpec()).extract();
 
-        AssertHelper.assertEquals(response.getStatusCode(), StatusCode.CREATED.code, "");
+        AssertHelper.assertEquals(response.getStatusCode(), StatusCode.CREATED.code,
+                "Verifying POST API status code");
         log.info("validatePostWithJsonFile executed successfully");
         log.info(response.getBody().asString());
+        JsonPath jsonPath = new JsonPath(response.getBody().asString());
+        jsonPath.getString("job");
+        log.info(jsonPath.getString("job"));
+        AssertHelper.assertEquals(jsonPath.getString("job"), "SoftwareQA",
+                "Verifying job profile ");
     }
 
     /**
@@ -130,15 +224,27 @@ public final class postUsers {
                 ExtentReport.extentreport.
                         startTest("validatePatchWithJsonFile",
                                 "validatePatchWithJsonFile");
-        Response response = given()
+//        Response response = given()
+//                .header("Content-Type", "application/json")
+//                .body(IOUtils.toString(fileInputStreamMethod("patchRequestBody.json")))
+//                .when()
+//                .patch("https://reqres.in/api/users" + "/2");
+        Response response = (Response) given().spec(requestSpec())
                 .header("Content-Type", "application/json")
                 .body(IOUtils.toString(fileInputStreamMethod("patchRequestBody.json")))
-                .when()
-                .patch("https://reqres.in/api/users" + "/2");
-        AssertHelper.assertEquals(response.getStatusCode(), StatusCode.SUCCESS.code, "");
+                .patch("/2").then().spec(responseSpec()).extract();
+
+        AssertHelper.assertEquals(response.getStatusCode(), StatusCode.SUCCESS.code,
+                "Verifying PATCH API status code");
         log.info("validatePatchWithJsonFile executed successfully");
         log.info(response.getBody().asString());
+        JsonPath jsonPath = new JsonPath(response.getBody().asString());
+        jsonPath.getString("name");
+        log.info(jsonPath.getString("name"));
+        AssertHelper.assertEquals(jsonPath.getString("name"), "pallavisawale",
+                "Verifying job profile ");
     }
+
     /**
      * API to validate PUT request using JSON in body part
      */
@@ -148,13 +254,24 @@ public final class postUsers {
                 ExtentReport.extentreport.
                         startTest("validatePutWithJsonFile",
                                 "validatePutWithJsonFile");
-        Response response = given()
+//        Response response = given()
+//                .header("Content-Type", "application/json")
+//                .body(IOUtils.toString(fileInputStreamMethod("putRequestBody.json")))
+//                .when()
+//                .put("https://reqres.in/api/users" + "/2");
+        Response response = (Response) given().spec(requestSpec())
                 .header("Content-Type", "application/json")
                 .body(IOUtils.toString(fileInputStreamMethod("putRequestBody.json")))
-                .when()
-                .put("https://reqres.in/api/users" + "/2");
-        AssertHelper.assertEquals(response.getStatusCode(), StatusCode.SUCCESS.code, "");
+                .put("/2").then().spec(responseSpec()).extract();
+
+        AssertHelper.assertEquals(response.getStatusCode(), StatusCode.SUCCESS.code,
+                "Verifying PUT API status code");
         log.info("validatePutWithJsonFile executed successfully");
         log.info(response.getBody().asString());
+        JsonPath jsonPath = new JsonPath(response.getBody().asString());
+        jsonPath.getString("job");
+        log.info(jsonPath.getString("job"));
+        AssertHelper.assertEquals(jsonPath.getString("job"), "SDET",
+                "Verifying job profile ");
     }
 }
